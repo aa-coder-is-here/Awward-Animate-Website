e878c96af26cdc7163a9f4f9a79e3473cd58822b
Ali Arif <aliarif46807@gmail.com>
2025-04-30 17:27:52 +0500

    All Done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,8 +15,9 @@ import TextHover from "@/Components/TextHover";
 import WorkSec from "@/Components/WorkSec";
 import Lenis from "lenis";
 import localFont from 'next/font/local'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PortCard from "@/Components/PortCards";
+import Loader from "@/Components/Loader";
 const clash = localFont({
   src: './fonts/ClashDisplay-Variable.ttf',
   display: 'swap',
@@ -24,6 +25,7 @@ const clash = localFont({
 })
 
 const Home = () => {
+  const [ loading , setLoading ] = useState(true);
   useEffect(()=>{
     const lenis = new Lenis();
     const raf = (time) => {
@@ -31,7 +33,26 @@ const Home = () => {
       requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
+
+    const handleLoad = () => setLoading(false);
+
+    if(document.readyState === "complete")
+       setLoading(false);
+    else
+      window.addEventListener("load", handleLoad);
+
+    return () => window.removeEventListener("load", handleLoad);
   },[])
+
+  if (loading) {
+    return (
+      <div className="transition-all duration-75 flex items-center justify-center h-screen bg-black text-white text-2xl">
+        <Loader />
+      </div>
+    );
+  }
+  
+
   return (
     <div className={`${clash.className} w-full min-h-screen h-auto relative`}>
       <Cursor />
